1fa9fd9c412c7838afc96b5d5ed10e50fd9488a3
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2022-12-28 00:46:16 +0800

    Create main1.cpp

---
+++ HW#6/main1.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 10;
+    n & (n-1) == 0 ? printf("true") : printf("false");
+
+    return 0;
+}
