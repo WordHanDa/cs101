800917002499f8fe9b6c6d921f9fb124bc332674
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2022-12-28 00:58:57 +0800

    Update main1.cpp

--- HW#6/main1.cpp
+++ HW#6/main1.cpp
@@ -1,19 +1,27 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-#include <iostream>
-
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
-    int n = 10;
-    n & (n-1) == 0 ? printf("true") : printf("false");
-
+   int i = 10;
+   if(i%3==0){
+       printf("Love\n");
+   }
+   if(i%5==0){
+       printf("IU\n");
+   }
+   if(i%15==0){
+       printf("Love IU\n");
+   }
+   if(i%3!=0 && i%5!=0 && i%15!=0){
+       printf("%d\n",i);
+   }
     return 0;
 }
