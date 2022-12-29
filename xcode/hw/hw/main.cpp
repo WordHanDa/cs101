d47fe4ca002ec2f68cc35e3e60b698dae807b143
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2022-12-30 03:28:21 +0800

    Update main.cpp

--- xcode/hw/hw/main.cpp
+++ xcode/hw/hw/main.cpp
@@ -1,28 +1,21 @@
 #include <stdio.h>
 int main() {
-    int rows = 4;
+    int rows = 7;
     int i, space, k = 0;
 
-    for (i = 1; i <= rows; ++i, k = 0) {
-       for (space = 1; space <= rows - i; ++space) {
+    for (i=1; i<= rows; ++i, k=0) {
+       for (space = 1; space <= rows - i; ++space)
           printf("  ");
-       }
-       while (k != 2 * i - 1) {
+       for(int k=0; k!=2*i-1; k++)
           printf("* ");
-          ++k;
-       }
        printf("\n");
     }
-    for(int i = 1; i<rows; i++){
-        printf("*");
-        for(int i = 1; i<=rows*3-1; i++){
-            printf(" ");
-        }
-        printf("*");
+    for(int i=0; i<rows; i++){
+        for(int i=1; i<=rows*2-1; i++)
+            (i==1||i==rows*2-1)?printf("* "):printf("  ");
         printf("\n");
     }
-    for(int i = 1; i<=rows*2-1; i++){
+    for(int i=1; i<=rows*2-1; i++)
         printf("* ");
-    }
     return 0;
 }
