27de4b6ef7c911599866a882270ba7338869673a
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2022-12-31 01:08:24 +0800

    Update main.cpp

--- xcode/hw/hw/main.cpp
+++ xcode/hw/hw/main.cpp
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#include <math.h>
-
 int main(){
-    int n = 1234;
-    int arry[10],i;
-    for(i=0; n>0; i++){
-        arry[i]=n%(pow(10,i));
-        n=n/(i*10);
+    int i, j;
+    printf("i=%d\n", j);//j=1
+    for(i=0; i<100,j!=3; i++, j++){
+        printf("i=%d\n", i);
     }
-    printf("%d",arry[2]);
-    /*for(i=i-1 ;i>=0 ;i--){
-        printf("%d",arry[i]);
-        i==4||i==8?printf(" "):printf("");
-    }*/
     return 0;
 }
-
+/*會，因為j預設是1，且不能等於3到了第四次迴圈就會達成條件直接跳出來。
+i=0,j=1
+ i=1,j=2;
+ i=2,j=3; !!
+ */
