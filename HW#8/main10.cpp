d651e64b7451fba89926bfdd36f3f1f09a919e52
Ipad <steven1428112@gmail.com>
2023-01-01 23:11:24 +0800

    main10 for

--- HW#8/main10.cpp
+++ HW#8/main10.cpp
@@ -4,10 +4,9 @@
 int main(){
     int n = 1234;
     int arry[10],i;
-    arry[0]=n/pow(10,0);
-    arry[1]=n/pow(10,1);
-    arry[2]=n/pow(10,2);
-    arry[3]=n/pow(10,3);
+    for(int i=0;i<=3;i++){
+        arry[i]=n/pow(10,i);
+    }
     for(i = 0; i < 4; i++){
         printf("%d",arry[i]%10);
     }
