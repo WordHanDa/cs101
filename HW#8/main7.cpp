09c5b26adf8158f6bd4b3492384b73f9707c01da
Ipad <steven1428112@gmail.com>
2023-01-02 18:19:14 +0800

    main7 for

--- HW#8/main7.cpp
+++ HW#8/main7.cpp
@@ -5,9 +5,9 @@ int main(){
     int n = -999;
     int arry[10],i,x,sum = 0;
     n<0?x=n*-1:x=n;
-    arry[0]=x/pow(10,0);
-    arry[1]=x/pow(10,1);
-    arry[2]=x/pow(10,2);
+    for(i=0;i<3;i++){
+        arry[i]=x/pow(10,i);
+    }
     n<0?arry[2]=arry[2]*-1:arry[2];
     for(i = 0; i < 3; i++){
         sum=arry[i]%10+sum;
