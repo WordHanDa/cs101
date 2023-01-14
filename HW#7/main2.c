3e160a5fe0dbafb7b5dc564c61cecd953dda35bf
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2023-01-15 01:58:29 +0800

    Update main2.c

--- HW#7/main2.c
+++ HW#7/main2.c
@@ -14,11 +14,11 @@ int main()
     double n = 0.0f;
     double i = 0.0f;
 
-    for(int k=1; k<=318000; k++){
+    for(int k=1; k<=272241; k++){
         i = n;
         n = 4*pow(-1,k+1)/(2*k-1);
         n = n+i;
-        //最小318000
+        //最小272241
     }
     printf("%f\n",n);
     
