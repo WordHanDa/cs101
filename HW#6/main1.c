d0bee6807efd59189f9debcaf126c8649268bccd
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2023-01-15 00:51:47 +0800

    Update main1.c

--- HW#6/main1.c
+++ HW#6/main1.c
@@ -13,14 +13,11 @@ int main()
    int i = 10;
    if(i%3==0){
        printf("Love\n");
-   }
-   if(i%5==0){
+   }else if(i%5==0){
        printf("IU\n");
-   }
-   if(i%15==0){
+   }else if(i%15==0){
        printf("Love IU\n");
-   }
-   if(i%3!=0 && i%5!=0 && i%15!=0){
+   }else if(i%3!=0 && i%5!=0 && i%15!=0){
        printf("%d\n",i);
    }
     return 0;
